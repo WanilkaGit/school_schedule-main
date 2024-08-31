0d618f03ef1b9c60bab27f37d8d43211504b9698
WanilkaGIT <mahlunivan27@gmail.com>
2024-08-31 20:18:43 +0300

    szdbxfcngvbhnm,

---
+++ myapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Subject, Teacher, Class, Student, Schedule, Grade
+# Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(Class)
+admin.site.register(Schedule)
+admin.site.register(Grade)
+# Register your models here.
